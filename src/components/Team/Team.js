0e22c41ab0111400492fcd41c5bdffbdeb8cb911
Priscillia <selvapriscillia@yahoo.fr>
2025-09-12 22:12:28 +0200

    fix security xss script

---
+++ src/components/Team/Team.js
@@ -0,0 +1,27 @@
+import { Swiper } from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const teamSwiper = new Swiper(".team-swiper", {
+  modules: [Navigation, Pagination],
+  slidesPerView: 1,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 1,
+    },
+    1024: {
+      slidesPerView: 2,
+    },
+  },
+});
