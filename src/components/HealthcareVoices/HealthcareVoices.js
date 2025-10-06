2feddedf963e418ebf472fb0820102798fc7d01e
Priscillia <selvapriscillia@yahoo.fr>
2025-10-06 19:00:44 +0200

    create section voices heathcare to page testimonials

---
+++ src/components/HealthcareVoices/HealthcareVoices.js
@@ -0,0 +1,29 @@
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+document.addEventListener("DOMContentLoaded", () => {
+  new Swiper(".healthcare-voices-swiper", {
+    modules: [Navigation, Pagination],
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 1,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+});
